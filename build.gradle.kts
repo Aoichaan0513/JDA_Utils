74aded1e4050f5d7d2a48569635b2e45a5298c7c
Aoichaan0513 <aoichaan0513@gmail.com>
2021-01-17 14:18:45 +0900

    [Change] WebhookUtil.kt での複数Embed対応

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 val groupId = "jp.aoichaan0513"
 group = groupId
-version = "1.0.9"
+version = "1.0.10"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/jp/aoichaan0513/JDA_Utils/WebhookUtil.kt
+++ src/main/kotlin/jp/aoichaan0513/JDA_Utils/WebhookUtil.kt
@@ -2,6 +2,7 @@ package jp.aoichaan0513.JDA_Utils
 
 import club.minnced.discord.webhook.WebhookClientBuilder
 import club.minnced.discord.webhook.receive.ReadonlyMessage
+import club.minnced.discord.webhook.send.WebhookEmbedBuilder
 import net.dv8tion.jda.api.entities.Message
 import net.dv8tion.jda.api.entities.MessageEmbed
 import net.dv8tion.jda.api.entities.Webhook
@@ -20,12 +21,23 @@ fun Webhook.send(msg: Message): ReadonlyMessage {
     return completableFuture.get()
 }
 
-fun Webhook.send(messageEmbed: MessageEmbed): ReadonlyMessage {
+fun Webhook.send(embed: MessageEmbed): ReadonlyMessage {
     val webhookClient = WebhookClientBuilder.fromJDA(this).buildJDA()
-    val completableFuture = webhookClient.send(messageEmbed)
+    val completableFuture = webhookClient.send(embed)
     webhookClient.close()
     return completableFuture.get()
 }
+
+fun Webhook.send(collection: Collection<MessageEmbed>): ReadonlyMessage {
+    val webhookClient = WebhookClientBuilder.fromJDA(this).buildJDA()
+    val completableFuture = webhookClient.send(collection.map { WebhookEmbedBuilder.fromJDA(it).build() })
+    webhookClient.close()
+    return completableFuture.get()
+}
+
+fun Webhook.send(iterable: Iterable<MessageEmbed>) = send(iterable.toList())
+
+fun Webhook.send(vararg embed: MessageEmbed) = send(embed.toList())
 
 
 fun Webhook.edit(readonlyMessage: ReadonlyMessage, content: String) = edit(readonlyMessage.id, content)
@@ -46,11 +58,31 @@ fun Webhook.edit(id: Long, msg: Message): ReadonlyMessage {
     return completableFuture.get()
 }
 
-fun Webhook.edit(readonlyMessage: ReadonlyMessage, messageEmbed: MessageEmbed) = edit(readonlyMessage.id, messageEmbed)
+fun Webhook.edit(readonlyMessage: ReadonlyMessage, embed: MessageEmbed) = edit(readonlyMessage.id, embed)
 
-fun Webhook.edit(id: Long, messageEmbed: MessageEmbed): ReadonlyMessage {
+fun Webhook.edit(id: Long, embed: MessageEmbed): ReadonlyMessage {
     val webhookClient = WebhookClientBuilder.fromJDA(this).buildJDA()
-    val completableFuture = webhookClient.edit(id, messageEmbed)
+    val completableFuture = webhookClient.edit(id, embed)
     webhookClient.close()
     return completableFuture.get()
 }
+
+fun Webhook.edit(readonlyMessage: ReadonlyMessage, collection: Collection<MessageEmbed>) =
+    edit(readonlyMessage.id, collection)
+
+fun Webhook.edit(id: Long, collection: Collection<MessageEmbed>): ReadonlyMessage {
+    val webhookClient = WebhookClientBuilder.fromJDA(this).buildJDA()
+    val completableFuture = webhookClient.edit(id, collection.map { WebhookEmbedBuilder.fromJDA(it).build() })
+    webhookClient.close()
+    return completableFuture.get()
+}
+
+fun Webhook.edit(readonlyMessage: ReadonlyMessage, iterable: Iterable<MessageEmbed>) =
+    edit(readonlyMessage.id, iterable.toList())
+
+fun Webhook.edit(id: Long, iterable: Iterable<MessageEmbed>) = edit(id, iterable.toList())
+
+fun Webhook.edit(readonlyMessage: ReadonlyMessage, vararg embed: MessageEmbed) =
+    edit(readonlyMessage.id, embed.toList())
+
+fun Webhook.edit(id: Long, vararg embed: MessageEmbed) = edit(id, embed.toList())
