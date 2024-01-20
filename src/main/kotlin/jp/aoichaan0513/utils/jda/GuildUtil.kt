22df75396b15ac37f39431725a98c432108fcd36
Aoichaan0513 <aoichaan0513@gmail.com>
2024-01-20 21:56:26 +0900

    [Update] GuildUtil.kt

--- src/main/kotlin/jp/aoichaan0513/utils/jda/GuildUtil.kt
+++ src/main/kotlin/jp/aoichaan0513/utils/jda/GuildUtil.kt
@@ -3,77 +3,21 @@ package jp.aoichaan0513.utils.jda
 import net.dv8tion.jda.api.entities.Guild
 import net.dv8tion.jda.api.entities.Member
 import net.dv8tion.jda.api.entities.Role
-import net.dv8tion.jda.api.entities.UserSnowflake
 
-fun Guild.addRolesToMember(member: Member, collection: Collection<Role>) {
-    collection.forEach { addRoleToMember(member, it).queue({}) {} }
-}
+fun Guild.addRolesToMember(member: Member, collection: Collection<Role>) =
+    modifyMemberRoles(member, collection, null)
 
-fun Guild.addRolesToMember(member: Member, iterable: Iterable<Role>) {
+fun Guild.addRolesToMember(member: Member, iterable: Iterable<Role>) =
     addRolesToMember(member, iterable.toSet())
-}
 
-fun Guild.addRolesToMember(member: Member, vararg array: Role) {
+fun Guild.addRolesToMember(member: Member, vararg array: Role) =
     addRolesToMember(member, array.toSet())
-}
 
-fun Guild.addRolesToMember(id: Long, collection: Collection<Role>) {
-    collection.forEach { addRoleToMember(UserSnowflake.fromId(id), it).queue({}) {} }
-}
+fun Guild.removeRolesFromMember(member: Member, collection: Collection<Role>) =
+    modifyMemberRoles(member, null, collection)
 
-fun Guild.addRolesToMember(id: Long, iterable: Iterable<Role>) {
-    addRolesToMember(id, iterable.toSet())
-}
-
-fun Guild.addRolesToMember(id: Long, vararg array: Role) {
-    addRolesToMember(id, array.toSet())
-}
-
-fun Guild.addRolesToMember(id: String, collection: Collection<Role>) {
-    collection.forEach { addRoleToMember(UserSnowflake.fromId(id), it).queue({}) {} }
-}
-
-fun Guild.addRolesToMember(id: String, iterable: Iterable<Role>) {
-    addRolesToMember(id, iterable.toSet())
-}
-
-fun Guild.addRolesToMember(id: String, vararg array: Role) {
-    addRolesToMember(id, array.toSet())
-}
-
-
-fun Guild.removeRolesFromMember(member: Member, collection: Collection<Role>) {
-    collection.forEach { removeRoleFromMember(member, it).queue({}) {} }
-}
-
-fun Guild.removeRolesFromMember(member: Member, iterable: Iterable<Role>) {
+fun Guild.removeRolesFromMember(member: Member, iterable: Iterable<Role>) =
     removeRolesFromMember(member, iterable.toSet())
-}
 
-fun Guild.removeRolesFromMember(member: Member, vararg array: Role) {
+fun Guild.removeRolesFromMember(member: Member, vararg array: Role) =
     removeRolesFromMember(member, array.toSet())
-}
-
-fun Guild.removeRolesFromMember(id: Long, collection: Collection<Role>) {
-    collection.forEach { removeRoleFromMember(UserSnowflake.fromId(id), it).queue({}) {} }
-}
-
-fun Guild.removeRolesFromMember(id: Long, iterable: Iterable<Role>) {
-    removeRolesFromMember(id, iterable.toSet())
-}
-
-fun Guild.removeRolesFromMember(id: Long, vararg array: Role) {
-    removeRolesFromMember(id, array.toSet())
-}
-
-fun Guild.removeRolesFromMember(id: String, collection: Collection<Role>) {
-    collection.forEach { removeRoleFromMember(UserSnowflake.fromId(id), it).queue({}) {} }
-}
-
-fun Guild.removeRolesFromMember(id: String, iterable: Iterable<Role>) {
-    removeRolesFromMember(id, iterable.toSet())
-}
-
-fun Guild.removeRolesFromMember(id: String, vararg array: Role) {
-    removeRolesFromMember(id, array.toSet())
-}
