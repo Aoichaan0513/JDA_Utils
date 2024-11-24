46febba8e88ec07f1e2f84d1db4c58bce81e6832
Aoichaan0513 <aoichaan0513@gmail.com>
2024-11-24 13:26:50 +0900

    [Upgrade] 依存関係の更新

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,9 +17,9 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.9.0")
 
-    implementation("net.dv8tion", "JDA", "5.1.0")
+    implementation("net.dv8tion", "JDA", "5.2.1")
     implementation("club.minnced", "discord-webhooks", "0.8.4")
-    implementation("joda-time", "joda-time", "2.12.7")
+    implementation("joda-time", "joda-time", "2.13.0")
     implementation("org.ocpsoft.prettytime", "prettytime", "5.0.9.Final")
     implementation("org.json", "json", "20240303")
     implementation("com.github.aoichaan0513", "Kotlin_Utils", "2.0.21_1")
